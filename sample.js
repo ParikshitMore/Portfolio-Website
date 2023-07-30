f37128ff792a8c9e5cd1495c66bcc005b3cc78b3
ParikshitMore <parikshitmore224@gmail.com>
2023-07-30 17:37:23 +0530

    First Commit

---
+++ sample.js
@@ -0,0 +1,6 @@
+//https://drive.google.com/file/d/1M4Rxob1ub0u3Iqs9q70Rq5xTS6ZUtSFD/view?usp=drive_link
+
+function cmd() {
+    
+    href = "https://github.com/ParikshitMore"; // I'm using example but you can use google.
+}
